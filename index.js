5cac6b2723dfbe57c72864129dfb4221831131d6
Brigitte Orina <brigitteorina@gmail.com>
2022-08-16 10:44:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+// Write your code here !
+let num1=2;
+let num2=31;
+
+const multiply =num1 * num2;
+
+const random = 2;
+
+let num3=24;
+let num4=6;
+
+const mod=num3/num4;
+let max =20;
